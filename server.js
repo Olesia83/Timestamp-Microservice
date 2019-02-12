34d2a075aa4afae03f2f2804feaa296dc6b0ae46
Olesia83 <47323414+Olesia83@users.noreply.github.com>
2019-02-12 15:26:44 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static(__dirname + '/public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+//your first API endpoint... 
+app.get("/api/timestamp", function (req, res){
+    var now=new Date();
+    res.send({"unix":now.getTime(),"utc":now.toUTCString()});
+  } );
+
+app.get("/api/timestamp/:date", function (req, res) {
+  var date = req.params.date;
+  var unix=Number(date);
+  var utc=new Date(unix);
+  var pUtc=new Date(date);
+  
+  if (pUtc.toString() !=='Invalid Date'){    
+      res.send({"unix":pUtc.getTime(),"utc":pUtc.toUTCString()});
+  }  
+  if ((unix===NaN) && (pUtc==='Invalid Date')){    
+      res.send({"unix":null,"utc":'Invalid Date'}); 
+  }  
+  else if (unix!==NaN){     
+      res.send({"unix":unix,"utc":utc.toUTCString()});
+  } 
+  
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
